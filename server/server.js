8d9da9ea958431f5fa6988b249f6c972006aa23c
Tbq2001 <thinhbuiquoc50@gmail.com>
2024-10-23 01:38:12 -0500

    update5

--- server/server.js
+++ server/server.js
@@ -1,22 +1,24 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const apiRoutes = require("./routes/api.js");
-const { getConnectionPool } = require("./database.js");
-const corsOptions = require("./config/corsOptions.js");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import apiRoutes from "./routes/api.js";
+import { getConnectionPool } from "./database.js";
+import corsOptions from "./config/corsOptions.js";
 
 dotenv.config(); // Load environment variables
 
 const app = express();
-const port = 5001;
+const port = 5001; // The port for the server
 
-app.use(cors(corsOptions));
-app.use(express.json());
+app.use(cors(corsOptions)); // Enable CORS for cross-origin requests
+app.use(express.json()); // Middleware to parse incoming JSON requests
+
 app.use("/api", apiRoutes);
 
+// Connect to the database
 getConnectionPool().catch((err) => {
   console.error("Failed to connect to DB", err);
-  process.exit(1);
+  process.exit(1); // Exit if the database connection fails
 });
 
 app.listen(port, () => {
